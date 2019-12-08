0b5e1676d32d7b9934ef3ee8754732973b7fb362
Michal <michal.olejko2@gmail.com>
2019-12-08 23:49:28 +0100

    added city model, and moving forward, back, rotate

--- src/enginetester/MainGameLoop.java
+++ src/enginetester/MainGameLoop.java
@@ -1,36 +1,54 @@
 package enginetester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
+import models.RawModel;
+import models.TextureModel;
 import renderengine.DisplayManager;
 import renderengine.ModelLoader;
-import renderengine.RawModel;
+import renderengine.OBJLoader;
 import renderengine.RenderModel;
+import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 
 		DisplayManager.createDisplay();
-
 		ModelLoader modelLoader = new ModelLoader();
-		RenderModel modelRenderer = new RenderModel();
+		StaticShader shader = new StaticShader();
+		RenderModel modelRenderer = new RenderModel(shader);
+		
 
-		float[] vertices = { -0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f,
 
-				0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f, -0.5f, 0.5f, 0f };
-
-		RawModel model = modelLoader.loadToVAO(vertices);
-
+		//RawModel model = modelLoader.loadToVAO(vertices, textureCoords, indices);
+		RawModel model = OBJLoader.loadObjModel("TheCity", modelLoader);
+		
+		//ModelTexture texture = new ModelTexture(modelLoader.loadTexture("image"));
+		//TextureModel texturedModel = new TextureModel(model, new ModelTexture(modelLoader.loadTexture("image")));
+		TextureModel cityModel = new TextureModel(model, new ModelTexture(modelLoader.loadTexture("cityTexture")));
+		//Entity entity = new Entity(texturedModel, new Vector3f(0,0,-1),0,0,0,1);
+		Entity entity = new Entity(cityModel, new Vector3f(0,-100,-1000),0,0,0,1);
+		
+		Camera camera = new Camera();
+		
 		while (!Display.isCloseRequested()) {
-
+			//entity.increasePosition(0, 0, -0.01f);
+			//entity.increaseRotation(0, 0.1f, 0);
+			camera.move();
 			modelRenderer.prepare();
-
-			modelRenderer.render(model);
-
+			shader.start();
+			shader.loadViewMatrix(camera);
+			modelRenderer.render(entity,shader);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
-
+		
+		shader.cleanUp();
 		modelLoader.cleanUp();
 		DisplayManager.closeDisplay();
 
